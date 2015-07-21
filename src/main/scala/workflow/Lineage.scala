c9f9e3e931e35ec1f927079daf34fc94f8b04136
Zhao Zhang <zhangzhao0718@gmail.com>
2015-07-20 22:02:46 -0700

    reimplementation to lincomlineage

--- src/main/scala/workflow/Lineage.scala
+++ src/main/scala/workflow/Lineage.scala
@@ -41,17 +41,17 @@ case class OneToOneLineage(inRows: Int, inCols: Int, outRows:Int, outCols: Int,
 				(outCols, seqSize) match {
 					case (1, 1) => {
 						//This is the case for matrix-to-vector
-						require((j*inRows+i < outRows), {"querying out of boundary of output"})
+						require((j*inRows+i < outRows), {"querying out of boundary of input vector"})
 						List((j*inRows+i))
 					}
 					case (_, 1) => {
 						//This is the case for matrix-to-matrix
-						require((i < outRows) && (j < outCols), {"querying out of boundary of output"})
+						require((i < outRows) && (j < outCols), {"querying out of boundary of input matrix"})
 						List((i, j))
 					}
 					case (1, _) => {
 						require((i < seqSize), {"Sequence index out of bound"})
-						require((inRows*i+j < outRows), {"querying out of boundary of output vector"})
+						require((inRows*i+j < outRows), {"querying out of boundary of input vector sequence"})
 						List(inRows * i + j)	
 					}
 				}
@@ -89,66 +89,104 @@ case class AllToOneLineage(inRows: Int, inCols: Int, outRows: Int, outCols: Int,
 		val k = key.getOrElse(null)
 		k match {
 			case i: Int =>{
-				require((inCols == 1), {"input is 1-d structure, use 1-d index"})
+				require((inCols == 1), {"input is 2-d structure, use 2-d index"})
 				imageMeta match {
 					//this is the case where input and output are images
 					case Some(meta) => {
-						//check condition here
-						List(i/outRows)
+						require((i < inRows), {"querying out of boundary of input vector"})
+						List(i%outRows)
 					}
-					//this is the case where output is an single element or 1-d vector
+					//this is the case where input and output are 1-d vectors
 					case _ => {
-						require((i < outRows), {"querying out of boundary of output vector"})
+						require((i < inRows), {"querying out of boundary of input vector"})
 						(0 until outRows).toList
 					}
 				}
 			}
 			case (i: Int, j: Int) =>{
-				require((inRows > 1)&&(inCols >1), {"input is 2-d structure, use 2-d index"})
+				require((inRows > 1)&&(inCols >1), {"input is 1-d structure, use 1-d index"})
+				require((i < inRows)&&(j < inCols), {"querying out of boundary of input matrix"})
 				val rSeq = for {
 					x <- 0 until outRows
 					y <- 0 until outCols
 				} yield (x, y)
 				rSeq.toList
 			}
+			case _ => List()
 		}
 	}
 
-	def getCoor(key: Int) = {
-		require((key < outRows), {"querying out of boundary of output vector"})
-		val rList = imageMeta match {
-			case Some(meta) => {
-				val xDim = meta.xDim
-				val yDim = meta.yDim
-				val numChannels = meta.numChannels
-				(0 until numChannels).toList.map(c => key+c*xDim*yDim)
+	def qBackward(key: Option[_]) = {
+		val k = key.getOrElse(null)
+		k match {
+			case i: Int =>{
+				require((outCols == 1), {"output is 2-d structure, use 2-d index"})
+				imageMeta match{
+					//this is the case where input and output are images
+					case Some(meta) => {
+						val xDim = meta.xDim
+						val yDim = meta.yDim
+						val numChannels = meta.numChannels
+						(0 until numChannels).toList.map(c => i+c*xDim*yDim)
+					}
+					case _ => (0 until inRows).toList
+				}
 			}
-			case None => {
-				(0 until inRows).toList
+			case (i: Int, j: Int) => {
+				require((inCols > 1), {"input is 1-d structure, use 1-d index"})
+				require((i < inRows)&&(j < inCols), {"querying out of boundary of input matrix"})
+				val rSeq = for {
+					i <- 0 until inRows
+					j <- 0 until inCols
+				} yield (i, j)
+				rSeq.toList
 			}
+			case _ => List()
 		}
-		rList
 	}
 
-	def getCoor2D(key: (Int, Int)) = {
-		val r = key._1
-		val c = key._2
-		require((r < outRows), {"querying out of boundary of output matrix"})
-		require((c < outCols), {"querying out of boundary of output matrix"})
-		val rSeq = for {
-			i <- 0 until inRows
-			j <- 0 until inCols
-		} yield (i, j)
-		rSeq.toList
-	}
+	def getCoor(key: Int) = List()
+
+	def getCoor2D(key: (Int, Int)) = List()
 }
 
 case class LinComLineage(inRows: Int, inCols: Int, outRows: Int, outCols: Int,
 	modelRows: Int, modelCols: Int, inRDDs: List[Int], outRDDs: List[Int]) extends Lineage{
-	/** 
-   *  The temporary implementation assumes the input and output are Vectors
-   *
-   */
+
+	def qBackward(key: Option[_]) = {
+		val k = key.getOrElse(null)
+		k match {
+			case i: Int =>{
+				require((outCols == 1)&&(inCols == 1), {"output is 2-d structure, use 2-d index"})
+				require((i < outRows), {"querying out of boundary of output vector"})
+				List(((0 until inRows).toList, (0 until inRows).toList.zip(List.fill(modelRows){i})))
+			}
+			case (i: Int, j: Int) => {
+				require((outCols > 1)&&(inCols > 1), {"output is 1-d structure, use 1-d index"})
+				require((i < outRows)&&(j < outCols), {"querying out of boundary of output matrix"})
+				List((List.fill(inCols){i}.zip((0 until inCols).toList), (0 until modelRows).toList.zip(List.fill(modelRows){j})))
+			}
+		}
+	}
+
+	def qForward(key: Option[_]) = {
+		val k = key.getOrElse(null)
+		k match {
+			case i: Int =>{
+				require((outCols == 1)&&(inCols == 1), {"input is 2-d structure, use 2-d index"})
+				require((i < inRows), {"querying out of boundary of input vector"})
+				(0 until outRows).toList
+			}
+			case (i: Int, j: Int) => {
+				require((outCols > 1)&&(inCols > 1), {"input is 1-d structure, use 1-d index"})
+				require((i < inRows)&&(j < inCols), {"querying out of boundary of input matrix"})
+				List.fill(outCols){i}.zip((0 until outCols).toList)
+			}
+			case _ => List()
+		}
+	}
+
+
 	def getCoor(key: Int): List[(List[Int], List[(Int, Int)])] = {
 		require((key < outRows), {"querying out of boundary of output vector"})
 		require((outCols == 1), {"output is a matrix, try use getCoor2D"})
