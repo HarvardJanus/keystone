a63dd729ef06d8a6d1788c90a329bb09ed6b4bea
Zhao Zhang <zhangzhao0718@gmail.com>
2015-08-19 11:57:44 -0700

    adding a comment for RTreeSuite

--- src/test/scala/workflow/RTreeSuite.scala
+++ src/test/scala/workflow/RTreeSuite.scala
@@ -5,6 +5,9 @@ import org.scalatest.FunSuite
 import pipelines.{LocalSparkContext, Logging}
 
 class RTreeSuite extends FunSuite with LocalSparkContext with Logging {
+	/**
+	 *	RTree requires the Box to be defined as (lowerLeftX, lowerLeftY, upperRightX, upperRightY)
+	 */
 	test("RTree test") {
 		val c1 = Entry(Box(0F, 0F, 1F, 1F), "alice")
 		val c2 = Entry(Box(0.4F, 0F, 1.4F, 1F), "bob")
