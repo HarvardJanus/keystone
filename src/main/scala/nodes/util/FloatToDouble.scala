f2b4014ad65fca505c9d0c0f7c57090f98bca230
Zhao Zhang <zhangzhao0718@gmail.com>
2015-08-27 23:38:09 -0700

    fix bug in FloatToDouble

--- src/main/scala/nodes/util/FloatToDouble.scala
+++ src/main/scala/nodes/util/FloatToDouble.scala
@@ -16,7 +16,7 @@ object FloatToDouble extends Transformer[DenseMatrix[Float], DenseMatrix[Double]
     val out = in.map(apply)
     val lineage = OneToOneLineage(in, out, this)
     lineage.save(tag)
-    println("collecting lineage for Transformer "+this.label+"\t mapping: "+lineage.qBackward((0, 0)))
+    println("collecting lineage for Transformer "+this.label+"\t mapping: "+lineage.qBackward((0, 0, 0)))
     out
   }
 }
