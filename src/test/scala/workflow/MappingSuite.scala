ee51dc173eed23c25ea1c4c0d7ef11815738c689
Zhao Zhang <zhangzhao0718@gmail.com>
2015-09-17 11:22:13 -0700

    adapt to latest mapping interface

--- src/test/scala/workflow/MappingSuite.scala
+++ src/test/scala/workflow/MappingSuite.scala
@@ -298,7 +298,6 @@ class MappingSuite extends FunSuite with Logging {
     assert(fResult == List(List((0,1), (0,2), (0,3), (1,1), (1,2), (1,3), (2,1), (2,2), (2,3))))
 
     val bResult = mapping.qBackward(Some(1, 2))
-    println(bResult)
     assert(bResult == List(List((0,2), (1,1), (1,2), (1,3), (2,0), (2,1), (2,2), (2,3), (2,4), (3,1), (3,2), (3,3), (4,2))))
 
     //wrong dimensional key
