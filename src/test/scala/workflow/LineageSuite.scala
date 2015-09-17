69c5c095dfbb1316d0b142ba5a0c42bd60f9db30
Zhao Zhang <zhangzhao0718@gmail.com>
2015-09-17 11:48:44 -0700

    add a note for NarrowLineage query interface

--- src/test/scala/workflow/LineageSuite.scala
+++ src/test/scala/workflow/LineageSuite.scala
@@ -16,27 +16,28 @@ class LineageSuite extends FunSuite with LocalSparkContext with Logging {
     val v = DenseVector.zeros[Double](5)
     val sRDD = sc.parallelize(List.fill(4){v})
     
-		val addition = new AdditionNode()
-		val tRDD = addition(sRDD)
+	val addition = new AdditionNode()
+	val tRDD = addition(sRDD)
 		
-		val lineage = OneToOneLineage(sRDD, tRDD, addition)
+	val lineage = OneToOneLineage(sRDD, tRDD, addition)
 
-		//query forward
-		assert(lineage.qForward(0, 2) == List((0, 2)))
-		//query backward
-		assert(lineage.qBackward(0, 2) == List((0, 2)))
-		//query forward out of bound of RDD
-		intercept[java.lang.IndexOutOfBoundsException] {
-			println(lineage.qForward(4, 2))
-		}
-		//query forward out of bound of a vector
-		intercept[org.apache.spark.SparkException] {
-			println(lineage.qForward(0, 5))
-		}
-		//query forward with wrong dimensional key
-		intercept[org.apache.spark.SparkException] {
-			println(lineage.qForward(0, 0, 0))
-		}
+	//query forward
+	assert(lineage.qForward(0, 2) == List((0, 2)))
+	//query backward
+	assert(lineage.qBackward(0, 2) == List((0, 2)))
+	//query forward out of bound of RDD
+	println(lineage.qForward(4,2))
+	intercept[java.lang.IndexOutOfBoundsException] {
+	  println(lineage.qForward(4, 2))
+	}
+	//query forward out of bound of a vector
+	intercept[org.apache.spark.SparkException] {
+	  println(lineage.qForward(0, 5))
+	}
+	//query forward with wrong dimensional key
+	intercept[org.apache.spark.SparkException] {
+	  println(lineage.qForward(0, 0, 0))
+	}
   }
 
   test("OneToOne Matrix Lineage") {
