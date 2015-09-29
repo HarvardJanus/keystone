4c69fbc88b58f2391d01e6d926bb4d33b0f754b6
Zhao Zhang <zhangzhao0718@gmail.com>
2015-09-28 23:38:40 -0700

    repalce count with collect in batch query

--- src/main/scala/workflow/Lineage.scala
+++ src/main/scala/workflow/Lineage.scala
@@ -388,7 +388,7 @@ object Lineage{
   def loadAndQueryBatch(paths: List[String], numQuery: Int, sc: SparkContext) = {
     val lineage = PipelineLineage(paths, sc)
     val list = List.fill(numQuery){(0,0)}.zip((0 until numQuery)).map(x=> (x._1._1, x._1._2, x._2))
-    val ellapsed = time(lineage.qBackwardBatch(sc.parallelize(list)).count)
+    val ellapsed = time(lineage.qBackwardBatch(sc.parallelize(list)).collect)
     println(ellapsed)
   }
 }
