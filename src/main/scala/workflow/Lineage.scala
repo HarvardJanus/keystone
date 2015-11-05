3aadbc4bb41971392f01e7d486065dd529d161f0
Zhao Zhang <zhangzhao0718@gmail.com>
2015-11-04 23:31:14 -0800

    add unpersist after storing

--- src/main/scala/workflow/Lineage.scala
+++ src/main/scala/workflow/Lineage.scala
@@ -198,14 +198,14 @@ case class NarrowLineage(inRDD: RDD[_], outRDD: RDD[_], mappingRDD: RDD[_], tran
       mappingRDD.saveAsObjectFile(path+"/"+tag+"/mappingRDD")
     }*/
     mappingRDD.saveAsObjectFile(path+"/"+tag+"/mappingRDD")
-    mappingRDD.unpersist()
+    mappingRDD.unpersist(true)
     val words = tag.split('_')
     if(words.size > 1 && words(0).startsWith("BkgSubstract")){
       inRDD.saveAsObjectFile(path+"/"+tag+"/inRDD")
     }
     outRDD.saveAsObjectFile(path+"/"+tag+"/outRDD")
-    inRDD.unpersist()
-    outRDD.unpersist()
+    inRDD.unpersist(true)
+    outRDD.unpersist(true)
   }
   def size = mappingRDD.count
 }
