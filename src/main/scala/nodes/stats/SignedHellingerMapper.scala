3b5117c5e53803a3439a1c53b8aba02e73b977cc
Zhao Zhang <zhangzhao0718@gmail.com>
2015-11-10 15:15:35 -0800

    small fix

--- src/main/scala/nodes/stats/SignedHellingerMapper.scala
+++ src/main/scala/nodes/stats/SignedHellingerMapper.scala
@@ -19,7 +19,7 @@ object SignedHellingerMapper extends Transformer[DenseVector[Double], DenseVecto
   override def saveLineageAndApply(in: RDD[DenseVector[Double]], tag: String): RDD[DenseVector[Double]] = {
     val out = in.map(apply)
     out.cache()
-    val lineage = OneToOneLineage(in, out, this)
+    val lineage = AllToOneLineage(in, out, this)
     lineage.save(tag)
     //println("collecting lineage for Transformer "+this.label+"\t mapping: "+lineage.qBackward(0, 0))
     out
