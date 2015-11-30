f2d054f558aa4301f0648d4fca346cdc5276b17e
Zhao Zhang <zhangzhao0718@gmail.com>
2015-11-30 14:25:14 -0600

    add lineage dir

---
+++ src/main/scala/lineage/Coordinate.scala
@@ -0,0 +1,22 @@
+package lineage
+
+abstract class Coor extends Serializable{
+}
+
+case class Coor1D(x: Int) extends Coor {
+  override def toString = x.toString
+}
+
+case class Coor2D(x:Int, y:Int) extends Coor {
+  override def toString = "("+x+", "+y+")"
+}
+
+case class Coor3D(x:Int, y:Int, c:Int) extends Coor {
+  override def toString = "("+x+", "+y+", "+c+")"
+}
+
+object Coor{
+  def apply(x:Int): Coor = new Coor1D(x)
+  def apply(x:Int, y:Int): Coor = new Coor2D(x, y)
+  def apply(x:Int, y:Int, c:Int): Coor = new Coor3D(x, y, c)
+}
