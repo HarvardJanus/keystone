245fdfe48d7d831e87e53681da618e68abff7d7c
Zhao Zhang <zhangzhao0718@gmail.com>
2015-12-11 14:59:26 -0800

    add changes to coordinate

--- src/main/scala/lineage/Coordinate.scala
+++ src/main/scala/lineage/Coordinate.scala
@@ -1,30 +1,52 @@
 package lineage
 
-abstract class Coor extends Serializable
+abstract class Coor extends Serializable{
+  def lower(): Coor
+  def raise(i: Int): Coor
+}
 
 
 case class Coor1D(x: Int) extends Coor {
   require(x>=0, {"coordinate can not be negative"})
   override def toString = x.toString
+  def lower() = Coor()
+  def raise(i: Int) = Coor(x, i)
 }
 
 case class Coor2D(x:Int, y:Int) extends Coor {
   require((x>=0)&&(y>=0), {"coordinate can not be negative"})
   override def toString = "("+x+","+y+")"
+  def lower() = Coor(y)
+  def raise(i: Int) = Coor(i, x, y)
 }
 
 case class Coor3D(x:Int, y:Int, c:Int) extends Coor {
   require((x>=0)&&(y>=0)&&(c>=0), {"coordinate can not be negative"})
   override def toString = "("+x+","+y+","+c+")"
+  def lower() = Coor(y, c)
+  def raise(i: Int) = Coor(i, x, y, c)
+}
+
+case class Coor4D(x:Int, y:Int, c:Int, d: Int) extends Coor {
+  require((x>=0)&&(y>=0)&&(c>=0)&&(d>=0), {"coordinate can not be negative"})
+  override def toString = "("+x+","+y+","+c+"d"+")"
+  def lower() = Coor(y, c, d)
+  /*
+   *  The following raise method is a place holder.
+   */
+  def raise(i: Int) = Coor(x, y, c, d)
 }
 
 case class CoorNull() extends Coor{
   override def toString = "null"
+  def lower() = this
+  def raise(i: Int) = Coor(i)
 }
 
 object Coor{
   def apply(x:Int): Coor = new Coor1D(x)
   def apply(x:Int, y:Int): Coor = new Coor2D(x, y)
   def apply(x:Int, y:Int, c:Int): Coor = new Coor3D(x, y, c)
+  def apply(x:Int, y:Int, c:Int, d:Int): Coor = new Coor4D(x, y, c, d)
   def apply() = new CoorNull()
 }
