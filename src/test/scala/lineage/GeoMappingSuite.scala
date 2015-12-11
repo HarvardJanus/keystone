086570ea77c563584efae25abcc31ac9519d8645
Zhao Zhang <zhangzhao0718@gmail.com>
2015-12-11 11:36:56 -0800

    add one more test case

--- src/test/scala/lineage/GeoMappingSuite.scala
+++ src/test/scala/lineage/GeoMappingSuite.scala
@@ -21,6 +21,5 @@ class GeoMappingSuite extends FunSuite with Logging {
 
     val bResult = mapping.qBackward(List(Coor(1, 2)))
     assert(bResult.toString == "List((0,2), (1,1), (1,2), (1,3), (2,0), (2,1), (2,2), (2,3), (2,4), (3,1), (3,2), (3,3), (4,2))")
-
   }
 }
