3681566d645c99d36c2a35be1e6d3ee05985e301
Zhao Zhang <zhangzhao0718@gmail.com>
2016-01-05 16:49:02 -0800

    more in lincom mapping and test cases

--- src/main/scala/lineage/LinComMapping.scala
+++ src/main/scala/lineage/LinComMapping.scala
@@ -8,6 +8,13 @@ case class LinComMapping(inSpace: SubSpace, outSpace: SubSpace) extends Mapping 
     require((flag==true), {"query out of subspace boundary"})
     (inSpace, outSpace) match {
       case (in: Vector, out: Vector) => out.expand()
+      case (in: Matrix, out: Matrix) => {
+        keys.flatMap(k=>{
+          val k2D = k.asInstanceOf[Coor2D]
+          val l = for (i <- 0 until out.yDim) yield Coor(k2D.x, i)
+          l.toList
+        })
+      }
     }
   }
 
@@ -16,6 +23,20 @@ case class LinComMapping(inSpace: SubSpace, outSpace: SubSpace) extends Mapping 
     require((flag==true), {"query out of subspace boundary"})
     (inSpace, outSpace) match {
       case (in: Vector, out: Vector) => in.expand()
+      case (in: Matrix, out: Matrix) => {
+        keys.flatMap(k=>{
+          val k2D = k.asInstanceOf[Coor2D]
+          val l = for (i <- 0 until in.yDim) yield Coor(k2D.x, i)
+          l.toList
+        })
+      }
     }
   }
+}
+
+object LinComMapping{
+  def apply(inVector: DenseVector[_], outVector: DenseVector[_]) = 
+    new LinComMapping(SubSpace(inVector), SubSpace(outVector))
+  def apply(inMatrix: DenseMatrix[_], outMatrix: DenseMatrix[_]) = 
+    new LinComMapping(SubSpace(inMatrix), SubSpace(outMatrix))
 }
--- src/test/scala/lineage/LinComMappingSuite.scala
+++ src/test/scala/lineage/LinComMappingSuite.scala
@@ -9,8 +9,16 @@ class LinComMappingSuite extends FunSuite with Logging {
   test("LinComMapping Vector Test"){
     val v1 = DenseVector.zeros[Double](4)
     val v2 = DenseVector.zeros[Double](4)
-    val mapping = LinComMapping(SubSpace(v1), SubSpace(v2))
+    val mapping = LinComMapping(v1, v2)
     assert(mapping.qForward(List(Coor(0))).toString == "List(0, 1, 2, 3)")
     assert(mapping.qBackward(List(Coor(0))).toString == "List(0, 1, 2, 3)")
+  }
+
+  test("LinComMapping Matrix Test"){
+    val m1 = DenseMatrix.zeros[Double](5, 4)
+    val m2 = DenseMatrix.zeros[Double](5, 2)
+    val mapping = LinComMapping(m1, m2)
+    assert(mapping.qForward(List(Coor(2,0))) == List(Coor(2,0), Coor(2,1)))
+    assert(mapping.qBackward(List(Coor(2,0))) == List(Coor(2,0), Coor(2,1), Coor(2,2), Coor(2,3)))
   }
 }
