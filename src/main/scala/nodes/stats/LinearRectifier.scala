88e5ca6b3bf40f98021ecc55027dc4fa02d1d66c
Zhao Zhang <zhangzhao0718@gmail.com>
2016-03-15 20:22:21 -0700

    randomfft now can record lineage with fine-grained stamp

--- src/main/scala/nodes/stats/LinearRectifier.scala
+++ src/main/scala/nodes/stats/LinearRectifier.scala
@@ -1,6 +1,8 @@
 package nodes.stats
 
 import breeze.linalg.DenseVector
+import lineage._
+import org.apache.spark.rdd.RDD
 import pipelines._
 import workflow.Transformer
 
@@ -13,5 +15,21 @@ case class LinearRectifier(maxVal: Double = 0.0, alpha: Double = 0.0)
   extends Transformer[DenseVector[Double], DenseVector[Double]] {
   def apply(in: DenseVector[Double]): DenseVector[Double] = {
     in.map(e => math.max(maxVal, e - alpha))
+  }
+
+  override def saveLineageAndApply(in: RDD[DenseVector[Double]], tag: String): RDD[DenseVector[Double]] = {
+    val stamp1 = System.nanoTime()
+    val out = in.map(apply)
+    out.cache()
+    out.count()
+    val stamp2 = System.nanoTime()
+    val lineage = IdentityLineage(in, out, this)
+    lineage.saveMapping(tag)
+    val stamp3 = System.nanoTime()
+    lineage.saveOutput(tag)
+
+    val stamp4 = System.nanoTime()
+    println(s"Transformer $tag: exec: ${(stamp2 - stamp1)/1e9}s, mapping: ${(stamp3-stamp2)/1e9}s, output: ${(stamp4-stamp3)/1e9}s")
+    out
   }
 }
--- src/main/scala/nodes/stats/RandomSignNode.scala
+++ src/main/scala/nodes/stats/RandomSignNode.scala
@@ -2,6 +2,8 @@ package nodes.stats
 
 import breeze.linalg._
 import breeze.stats.distributions._
+import lineage._
+import org.apache.spark.rdd.RDD
 import workflow.Transformer
 
 /**
@@ -13,6 +15,21 @@ case class RandomSignNode(signs: DenseVector[Double])
 
   def apply(in: DenseVector[Double]): DenseVector[Double] = in :* signs
 
+  override def saveLineageAndApply(in: RDD[DenseVector[Double]], tag: String): RDD[DenseVector[Double]] = {
+    val stamp1 = System.nanoTime()
+    val out = in.map(apply)
+    out.cache()
+    out.count()
+    val stamp2 = System.nanoTime()
+    val lineage = IdentityLineage(in, out, this)
+    lineage.saveMapping(tag)
+    val stamp3 = System.nanoTime()
+    lineage.saveOutput(tag)
+
+    val stamp4 = System.nanoTime()
+    println(s"Transformer $tag: exec: ${(stamp2 - stamp1)/1e9}s, mapping: ${(stamp3-stamp2)/1e9}s, output: ${(stamp4-stamp3)/1e9}s")
+    out
+  }
 }
 
 object RandomSignNode {
