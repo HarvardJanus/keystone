fd2f448aa23aafd867b8889089e0a5747f2ce2c4
Zhao Zhang <zhangzhao0718@gmail.com>
2016-03-29 15:19:29 -0700

    add composite lineage backward query

--- src/main/scala/lineage/Lineage.scala
+++ src/main/scala/lineage/Lineage.scala
@@ -100,7 +100,38 @@ case class CompositeLineage(lineageSeq: Seq[NarrowLineage]) extends Queriable{
     }
   }
 
-  def qBackward(keys: List[Coor]) = List(Coor(0))
+  def qBackward(keys: List[Coor]) = {
+    val i = keys.head.first
+    val resultRDD = mergedMappingRDD.zipWithIndex.map{
+      case (mappingSeq, index) => {
+        if(index == i){
+          qBackwardRecursive(keys.map(_.lower), mappingSeq.reverse)
+        }
+      }
+    }
+
+    val filteredRDD = resultRDD.zipWithIndex.filter{
+      case (result, index) => (index == i)
+    }.map(_._1)
+
+    val m = filteredRDD.first
+    val innerRet = m.asInstanceOf[List[Coor]]
+
+    if(innerRet.size == 0) 
+      List(Coor())
+    else
+      innerRet.map(_.raise(i))
+  }
+
+  def qBackwardRecursive(keys: List[Coor], mappingSeq: Seq[Mapping]): List[Coor] = {
+    mappingSeq match {
+      case Nil => keys
+      case head::tail => {
+        val interResults = head.qBackward(keys)
+        qBackwardRecursive(interResults, tail)
+      }
+    }
+  }
 }
 
 case class NarrowLineage(inRDD: RDD[_], outRDD: RDD[_], mappingRDD: RDD[_], transformer: Transformer[_,_], model: DenseMatrix[_]=null) extends Lineage{
